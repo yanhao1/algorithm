6580422764482289fea76fac2442544552045e6c
严豪 <fangtu@7lk.com>
2018-09-17 15:21:02 +0800

    快速排序

--- quicksort/golang/quickSort.go
+++ quicksort/golang/quickSort.go
@@ -1,1 +1,29 @@
 package main
+
+import "fmt"
+
+func quicksort(list []int) []int {
+	if len(list) < 2 {
+		return list
+	} else {
+		pivot := list[0]
+
+		var less = []int{}
+		var greater = []int{}
+		for _, num := range list[1:] {
+			if pivot > num {
+				less = append(less, num)
+			} else {
+				greater = append(greater, num)
+			}
+		}
+
+		less = append(quicksort(less), pivot)
+		greater = quicksort(greater)
+		return append(less, greater...)
+	}
+}
+
+func main()  {
+	fmt.Println(quicksort([]int{10,2,3,45,62,1,28,222}))
+}
